8ef65aa45617a305721175019df8e2b81b29395a
Adedayoke <adedayoke2006@gmail.com>
2024-08-19 17:53:38 +0100

    two sum completed

---
+++ Two sum/two_sum.js
@@ -0,0 +1,55 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+function twoSum(arr, tar){
+    for(let i = 0; i < arr.length; i++){
+        var sum = arr[i]
+        for(let j = 1; j < arr.length; j++){
+            if(sum + arr[j] === tar){
+                return ([i, j])
+            }
+        }
+    }
+}
+
+
+// case 1
+// const nums = [2,7,11,15]
+// const target = 9
+
+// case 1
+// const nums = [3,2,4]
+// const target = 6
+
+// case 3
+const nums = [3,3]
+const target = 6
+
+console.log(twoSum(nums, target))
